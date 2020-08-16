d5cba63e73685e1ab8424cb93e4edd72a98bb19a
Nick-Kh <many_sided@hotmail.com>
2020-08-16 03:32:46 +0300

    added fb share feature

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -221,6 +221,39 @@ const onSaveMeme = () => {
   let memeId = gMemeId++
   let memeData = { imageData, memeId, ...gCurrMeme }
   addNewMeme(memeData)
+  var elSuccessModal = document.querySelector('.success-modal')
+  elSuccessModal.style.display = 'block'
+  setTimeout(function () {
+    elSuccessModal.style.display = 'none'
+  }, 1500)
 }
 
-const onShareMeme = () => {}
+function onShareMeme(elForm, ev) {
+  ev.preventDefault()
+  document.getElementById('imgData').value = gCanvas.toDataURL('image/jpeg')
+
+  // A function to be called if request succeeds
+  function onSuccess(uploadedImgUrl) {
+    uploadedImgUrl = encodeURIComponent(uploadedImgUrl)
+    window.open(
+      `https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}`
+    )
+  }
+
+  doUploadImg(elForm, onSuccess)
+}
+
+function doUploadImg(elForm, onSuccess) {
+  var formData = new FormData(elForm)
+  fetch('http://ca-upload.com/here/upload.php', {
+    method: 'POST',
+    body: formData,
+  })
+    .then(function (res) {
+      return res.text()
+    })
+    .then(onSuccess)
+    .catch(function (err) {
+      console.error(err)
+    })
+}
